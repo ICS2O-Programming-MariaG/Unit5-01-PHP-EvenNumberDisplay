2d25d589565f0a23bae40637403e75a3eb8e1880
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-19 00:09:46 +0000

    Updated numbers.php to create error message when $minNumber > $maxNumber

--- numbers.php
+++ numbers.php
@@ -1,6 +1,7 @@
 <?php
-  //initializing the numbers variable to an empty string
+  //initializing the numbers and message variable to an empty string
   $numbers = "";
+  $message = "";
 
   //getting the user values for the minimum and maximum numbers
   $minNumber = intval($_POST['min-number']);
@@ -9,16 +10,24 @@
   //initializing the counter variable to the minNumber
   $counter = $minNumber;
 
-  //while loop that specifies the range, so the code within the loop executes as long as the counter is greater than or equal to the minimum and less than or equal to the maximum
-  while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
-    //if statement checks for even numbers, for as long as the counter is still less than or equal to the maximum
-    //number % 2 == 0 makes sure remainder is 0, indicating that number is divisible by 2 (even number)
-    if (($counter <= $maxNumber) && ($counter % 2 == 0)) {
-      $numbers = $numbers . $counter . "<br>";
+  //if statement checks if minNumber > maxNumber (there is an error)
+  if ($minNumber > $maxNumber) {
+    $message = "Please make sure the minimum is less than or equal to the maximum.";
+  }
+  //else, complete while loop to create list of even numbers between min and max
+  else {
+    //while loop that specifies the range, so the code within the loop executes as long as the counter is greater than or equal to the minimum and less than or equal to the maximum
+    while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
+      //if statement checks for even numbers, for as long as the counter is still less than or equal to the maximum
+      //number % 2 == 0 makes sure remainder is 0, indicating that number is divisible by 2 (even number)
+      if (($counter <= $maxNumber) && ($counter % 2 == 0)) {
+        $numbers = $numbers . $counter . "<br>";
+      }
+      $counter = $counter + 1;
+      $message = "Here is a list of all the even numbers between the minimum and maximum that you entered:<br><br>$numbers";
     }
-    $counter = $counter + 1;
   }
 
   //displaying the results (the range of even numbers) back to the user
-  echo "Here is a list of all the even numbers between the minimum and maximum that you entered:<br><br>$numbers";
+  echo "$message";
 ?>
